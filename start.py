dc492ab5cc5a08e9e4400ba2f67fd5c809904d12
wultes <workwultes@gmail.com>
2020-04-04 16:12:01 +0300

    modifty utils

--- start.py
+++
@@ -1,5 +0,0 @@
-from VkBot.bot import MakeBot
-
-if __name__ == "__main__":
-    bot = MakeBot()
-    bot.start_listen()
--- utils/config_tool.py
+++ utils/config_tool.py
@@ -1,15 +1,13 @@
 import toml
 
-CONFIG_COMMAND = ['token']
-
-def get_config():
+def get_config(path_config):
 
     config = {}
 
-    with open('VkBot/config.toml', 'r', encoding='utf-8') as config_file:
-        for line in CONFIG_COMMAND:
+    with open(path_config, 'r', encoding='utf-8') as config_file:
+        for line in config_file:
             if line not in config:
                 for key, value in toml.load(config_file).items():
                     config[key] = value
-    
+
     return config
