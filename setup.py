b84d6da8283f169ad2fd1953254921b30d1be95a
wultes <workwultes@gmail.com>
2020-05-06 09:30:11 +0300

    new version 0.1.8

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 setup(
     name='vkbotfather',
     description='Father for creating VK Bots',
-    version='0.1.7',
+    version='0.1.8',
     url='https://github.com/wultes/vkbotfather',
     license='MIT',
     author='Wultes (Kirill)',
     author_email='wultesu@gmail.com',
     packages=['vkbotfather', 'vkbotfather.fatherbot', 'vkbotfather.fatherbot', 'vkbotfather.fatherbot.models', 'vkbotfather.utils', 'vkbotfather.plugins'],
-    install_requires=['toml', 'Pillow']
+    install_requires=['toml', 'Pillow', 'requests']
 )
