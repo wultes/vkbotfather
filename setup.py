8fc4d9a84e6da8022369b413dd81c2e6e5755c33
wultes <workwultes@gmail.com>
2020-05-30 15:16:34 +0300

    New version 0.2.9

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vkbotfather',
+    description='Father for creating VK Bots',
+    version='0.2.9',
+    url='https://github.com/wultes/vkbotfather',
+    license='MIT',
+    author='Wultes (Kirill)',
+    author_email='wultesu@gmail.com',
+    packages=find_packages(),
+    install_requires=['toml', 'Pillow', 'requests'],
+)
